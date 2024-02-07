f56e88cf6461e43d52942dce9170fdd6ec3c75f2
jeffreythomas <jeffreykthomas@gmail.com>
2024-02-07 15:27:10 -0800

    Update slack app, add procfile

--- slack_app/application.py
+++ slack_app/application.py
@@ -1,32 +1,54 @@
 import os
+import logging
+import json
+import boto3
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-import boto3
-import json
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
+
+if not SLACK_BOT_TOKEN or not SLACK_APP_TOKEN:
+    logger.error("SLACK tokens not found in environment variables")
+    raise ValueError("SLACK tokens not found")
 
 # Initialize the app
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"), app_token=os.environ.get("SLACK_APP_TOKEN"))
+app = App(token=SLACK_BOT_TOKEN)
 
 
 # Handle a message shortcut
 @app.shortcut("analyze_message")
 def handle_shortcut(ack, shortcut, client):
-	ack()
+    ack()
 
-	message_text = shortcut['message']['text']
+    message_text = shortcut['message']['text']
+    logger.info(f"Received shortcut request with message: {message_text}")
 
-	sagemaker_client = boto3.client('sagemaker-runtime', region_name='us-west-2')
-	response = sagemaker_client.invoke_endpoint(
-		EndpointName='emotion-ai-endpoint',
-		ContentType='application/json',
-		Body=json.dumps({"text": [message_text]})
-	)
-	inference_result = json.load(response['Body'])
+    sagemaker_client = boto3.client('sagemaker-runtime', region_name='us-west-2')
+    payload = json.dumps({"text": message_text})
+    logger.info(f"Invoking SageMaker endpoint with payload: {payload}")
 
-	# Respond in Slack
-	client.chat_postMessage(channel=shortcut['user']['id'], text=f"Prediction result: {inference_result}")
+    response = sagemaker_client.invoke_endpoint(
+        EndpointName='emotion-ai-endpoint',
+        ContentType='application/json',
+        Body=payload
+    )
+    inference_result = json.load(response['Body'][0])
+    rounded_result = {label: round(percentage * 100, 2) for label, percentage in inference_result.items()}
+    logger.info(f"Received inference result from SageMaker: {rounded_result}")
+
+    # Respond in Slack
+    result_message = f"Prediction result: {rounded_result}"
+    client.chat_postMessage(channel=shortcut['channel']['id'], text=result_message)
+    logger.info(f"Successfully sent message to Slack: {result_message}")
 
 
 # Start your app
 if __name__ == "__main__":
-	SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    logger.info("Starting Slack app in Socket Mode")
+    SocketModeHandler(app, SLACK_APP_TOKEN).start()
+
